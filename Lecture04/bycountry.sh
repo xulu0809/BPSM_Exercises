99ad82e3712a2c48537bc4c7e1ad74bf846dc857
xulu0809 <xu_lu2019@163.com>
2019-09-29 12:14:05 +0100

    Added the bycountry.sh file from Lecture04

---
+++ Lecture04/bycountry.sh
@@ -0,0 +1,14 @@
+rm -f *.details
+count=0
+unset IFS
+IFS=$'\t'
+while read name email city birthday_day birthday_month birthday_year country
+do
+if test -z $name || test $country == "country"
+ then
+ continue
+ else
+ count=$((count+1))
+ echo -e "$count\t$name\t$city\t$country" >> ${country// /}.details
+fi
+done < example_people_data.tsv
