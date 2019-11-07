9413368b48b9ca6ee8feb4331a463eb2c58500e9
xulu0809 <xu_lu2019@163.com>
2019-11-07 19:28:51 +0000

    Finished clustalo in assignment2.py for assignment2, added some codes to ass2.draft.

--- ass2/assignment2.py
+++ ass2/assignment2.py
@@ -11,13 +11,13 @@ organism = "aves"
 #need delete space?
 file_name = protein_name+"_"+organism+".fa"
 # run in unix command line; bytes -> string
-def shell_run(command):
+def shell_out(command):
 	output = subprocess.check_output(command,shell=True).decode()
 	return output
 
 #could be in a function?
 cmd_search = "esearch -db protein -query '"+protein_name+"[PROT] AND "+organism+"[ORGN] NOT partial NOT predicted'"
-info = shell_run(cmd_search).split()
+info = shell_out(cmd_search).split()
 count = (info[4])[7:-8]
 
 if count == "0":
@@ -27,7 +27,7 @@ if count == "0":
 
 print("There are "+count+" results found.")
 cmd_fetch = cmd_search+" | efetch -db protein -format fasta"
-sequence = shell_run(cmd_fetch)
+sequence = shell_out(cmd_fetch)
 fasta_file = open(file_name,"w")
 fasta_file.write(sequence)
 fasta_file.close()
@@ -35,10 +35,23 @@ fasta_file.close()
 fastas = sequence.split("\n")
 list_species = []
 for line in fastas:
-	if line.find(">") == 0:
+	if line.startswith(">"):
 		species = line[line.find("[")+1:line.find("]")]
 		list_species.append(species)
 all_species = set(list_species)
-print("There are "+str(len(all_species))+" species:\n"+"\n".join(all_species))
+# sort? how many for each species?
+print("There are "+str(len(all_species))+" species:\n"+", ".join(all_species))
 # continue or not?
+def ifcontinue():
+	y_n = input("Do you want to continue? (y/n): ")
+	if y_n.upper() == "N":
+		print("Quiting...")
+		exit()
+	elif y_n.upper() != "Y":
+		print("Sorry, you can only use 'y' or 'no'.")
+		ifcontinue()
+ifcontinue()
+
+subprocess.call("clustalo -i "+file_name+" -o clus_out.fa -v --force --outfmt clu",shell=True)
+
 
