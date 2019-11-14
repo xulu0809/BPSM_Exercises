af8ac1d89edb3e12eec7039517be0f5daf05fe4f
xulu0809 <xu_lu2019@163.com>
2019-11-14 22:29:06 +0000

    Add some codes for selecting 250 sequences in assignment2.py

--- ass2/assignment2.py
+++ ass2/assignment2.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import os
+import os,sys
 import subprocess
+# Get information from user's input
+# use AND to search for more items?
 # capitalize...
-#protein_name = input("Please enter the protein name: ")
-#organism = input("Please enter the organism: ")
-
-protein_name = "glucose-6-phosphatase"
-organism = "aves".capitalize()
+protein_name = input("Please enter the protein name: ")
+organism = input("Please enter the organism: ")
+#protein_name = "glucose-6-phosphatase"
+#organism = "aves"
 
 #need delete space?
 file_name = protein_name+"_"+organism
@@ -14,7 +15,7 @@ file_name = protein_name+"_"+organism
 def shell_out(command):
 	output = subprocess.check_output(command,shell=True).decode()
 	return output
-'''
+
 #could be in a function?
 cmd_search = "esearch -db protein -query '"+protein_name+"[PROT] AND "+organism+"[ORGN] NOT partial NOT predicted'"
 info = shell_out(cmd_search).split()
@@ -26,20 +27,25 @@ if count == "0":
 	exit()
 
 print("There are "+count+" results found.")
-cmd_fetch = cmd_search+" | efetch -db protein -format fasta"
-sequence = shell_out(cmd_fetch)
-fasta_file = open(file_name+".fa","w")
-fasta_file.write(sequence)
-fasta_file.close()
-
-fastas = sequence.split("\n")
+cmd_fetch = cmd_search+" | efetch -db protein -format fasta >"+file_name+".fa"
+#sequence = shell_out(cmd_fetch)
+os.system(cmd_fetch)
+raw_fasta = open(file_name+".fa")
+sequences = {} #dictionary for raw sequence fasta
 list_species = []
-for line in fastas:
+for line in raw_fasta:
 	if line.startswith(">"):
+		name = line.split()[0].replace(">","")
 		species = line[line.find("[")+1:line.find("]")]
 		list_species.append(species)
+		sequences[name] = line
+	else:
+		sequences[name] += line.replace("\n","")
+
 all_species = set(list_species)
 #sort? how many for each species?
+
+
 print("There are "+str(len(all_species))+" species:\n"+", ".join(all_species))
 
 # continue or not?
@@ -53,9 +59,39 @@ def ifcontinue():
 		ifcontinue()
 ifcontinue()
 
-subprocess.call("clustalo -i "+file_name+".fa -o clus_out.fa -outfmt fasta --force",shell=True)
-'''
-subprocess.call("plotcon -winsize 4 -graph svg -gsubtitle '"+protein_name+" in "+organism+"' -goutfile "+file_name+" clus_out.fa;display "+file_name+".svg",shell=True)
+cmd_clus = "clustalo -i "+file_name+".fa -o "+file_name+"_clus.fa --outfmt fasta -v --threads=256 --force"
 
+#5 -> 250
+if len(sequences) > 20:
+	cmd_cons = "cons -sequence "+file_name+"_clus.fa -outseq "+file_name+"_cons.fa"
+	cmd_blastdb = "makeblastdb -in "+file_name+".fa -dbtype prot -out "+file_name
+	cmd_blastp = "blastp -db "+file_name+" -query "+file_name+"_cons.fa -outfmt 7 > blastp.out"
+	#subprocess.call(cmd_clus+";"+cmd_cons+";"+cmd_blastdb+";"+cmd_blastp,shell=True)
+	os.system(cmd_clus)
+	os.system(cmd_cons)
+	os.system(cmd_blastdb)
+	os.system(cmd_blastp)
+	#blastout = shell_out(cmd_blastp)
+	blastp = open("blastp.out")
+	#blastp.write(blastout)
+	#blastp.close()
+	acc250 = []
+	count = 0
+	for line in blastp:
+		if line.startswith("#") == False and count < 20:
+			count += 1
+			acc250.append(line.split()[1])
+	sequences250 = {}
+	file250 = open("seq250.fa","a")
+	for acc in acc250:
+		seq_in250 = sequences.get(acc)
+		sequences250[acc] = seq_in250
+		file250.write(seq_in250.replace(">","\n>"))
+	file250.close()
+	#delete the raw sequence? rename the 250 sequences
+	os.remove(file_name+".fa")
+	os.rename("seq250.fa",file_name+".fa")
+	os.system(cmd_clus)
+os.system("plotcon -winsize 4 -graph svg -gsubtitle '"+protein_name+" in "+organism+"' -goutfile "+file_name+" "+file_name+"_clus.fa;display "+file_name+".svg")
 
 
